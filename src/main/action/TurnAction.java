6950ae03e55f6b0794a672b6ba610f20bc0dac89
Grafstain <grafstain@gmail.com>
2024-07-23 15:45:30 +0300

    add processCreatureMoves method to TurnAction class

--- src/main/action/TurnAction.java
+++ src/main/action/TurnAction.java
@@ -1,6 +1,24 @@
 package main.action;
 
 
-public class TurnAction extends Action {
+import main.Map;
+import main.entity.Entity;
+import main.entity.creature.Creature;
 
+public class TurnAction extends Action {
+    /**
+     Processes the moves of all creatures on the map.
+     @param mapObj The map object which contains all the entities, including the creatures.
+     */
+    public void processCreatureMoves(Map mapObj) {
+        Entity[][] map = mapObj.getMap();
+
+        for (Entity[] entities : map) {
+            for (Entity entity : entities) {
+                if (entity instanceof Creature creature) {
+                    creature.makeMove();
+                }
+            }
+        }
+    }
 }
