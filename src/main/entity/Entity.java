494f60cd68d1dabb208a214bada7d1314d860abd
Grafstain <grafstain@gmail.com>
2024-07-23 15:20:33 +0300

    add getters & setters for Coordinates field in Entity class

--- src/main/entity/Entity.java
+++ src/main/entity/Entity.java
@@ -1,4 +1,15 @@
 package main.entity;
 
+import main.Coordinates;
+
 public abstract class Entity {
+    private Coordinates coordinates;
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+        this.coordinates = coordinates;
+    }
 }
